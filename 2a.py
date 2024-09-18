7fd7befe9686accad3aa4cb68c3f4eb3c9babc57
varshu234 <varshashivakumar36@gmail.com>
2024-09-18 12:55:19 +0530

    Add files via upload

---
+++ 2a.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if(n==0 or n==1):
+          return n
+    else:
+        return(fib(n-1)+fib(n-2))
+num=int(input("Enter number"))
+if(num>=0):
+        f=fib(num)
+        print("fib Sequenceis",f)
+else:
+        print("Not a valid number")
