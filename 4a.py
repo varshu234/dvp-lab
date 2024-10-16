af9b63ec04bf0e2793588383d743aaad32fda05e
varshu234 <varshashivakumar36@gmail.com>
2024-10-16 12:48:10 +0530

    Update 4a.py

--- 4a.py
+++ 4a.py
@@ -4,5 +4,5 @@ values=[500,600,620,680]
 plt.bar(std,values,color='blue')
 plt.xlabel("education", fontsize=12)
 plt.ylabel("marks obtained",fontsize=12)
-plt.title("ABC-123")
+plt.title("varsha s 1KI23CS176")
 plt.show()
