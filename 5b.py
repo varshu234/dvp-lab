0a9e7da680cb5102025a1f54f5106e2feafd1018
varshu234 <varshashivakumar36@gmail.com>
2024-10-16 12:37:05 +0530

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['dester', 'rangerover','swift','scorpio','kia','benz']
+data=[30,40,50,20,60,90]
+plt.figure(figure=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("varsha s 1KI23CS176")
+plt.show()
