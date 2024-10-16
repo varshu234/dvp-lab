40bdce646f8013cc5f8cee34395848dbcf464039
varshu234 <varshashivakumar36@gmail.com>
2024-10-16 12:08:38 +0530

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3]
+plt.scatter(x,y,color='yellow')
+plt.xlabel("x axis",fontsize=12)
+plt.ylabel("y axis",fontsize=12)
+plt.title("varsha ha1ki23cs175")
+plt.show()
