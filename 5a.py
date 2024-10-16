009835f49553ced63ee543361d51d522a99e1bd9
varshu234 <varshashivakumar36@gmail.com>
2024-10-16 12:26:52 +0530

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.hist([11,13,15,16,18,20,22,24,34,45,12,12],bins=4)
+plt.xlabel("Distribution of age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("varsha s 1KI23CS176")
+plt.show()
