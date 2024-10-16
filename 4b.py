e06ad522aecb42895ba2b7c287e1f178b2de4f90
varshu234 <varshashivakumar36@gmail.com>
2024-10-16 12:10:08 +0530

    Update 4b.py

--- 4b.py
+++ 4b.py
@@ -4,5 +4,5 @@ y=[-1,0,1,2,3]
 plt.scatter(x,y,color='yellow')
 plt.xlabel("x axis",fontsize=12)
 plt.ylabel("y axis",fontsize=12)
-plt.title("varsha ha1ki23cs175")
+plt.title("varsha s 1KI23CS176")
 plt.show()
